9decaab9aadb5e09e8fa5a498ce13054b32de062
rjw1428 <rjw1428@gmail.com>
2019-05-03 07:04:29 -0400

    resizing image cloud function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ItemsServiceService } from './items-service.service';
+import { Item } from './item/item.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  timeDelay = 1; //IN MINUTES
+  resetHour = 13;
+  editForm = false;
+  editItem: Item;
+  constructor(private service: ItemsServiceService) {
+
+  }
+
+  ngOnInit() {
+    var time = Observable.interval(60000 * this.timeDelay)
+      .subscribe(x => {
+        var date = new Date()
+        if (date.getHours() == this.resetHour && date.getMinutes() >= 0) {
+          console.log("COUNTDOWN TO REFRESH STARTED")
+          setTimeout(() => this.refresh(), 60000 * 100);
+        }
+      })
+  }
+
+  refresh() {
+    window.location.reload(true)
+  }
+
+  parseCharacters(title: string) {
+    let result = title.toLowerCase()
+    result = result.replace(/ü/g, "u")
+    result = result.replace(/ä/g, "a")
+    result = result.replace(/ö/g, "o")
+    result = result.replace(/ß/g, "ss")
+    result = result.replace(/ñ/g, "n")
+    result = result.replace(/ë/g, "e")
+    console.log(result)
+  }
+
+  closeEdit(item: { item: Item }) {
+    this.editForm = false;
+    //this.service.itemEdit()
+  }
+
+  openEdit(item: { item: Item }) {
+    this.editItem = item.item;
+    this.editForm = true;
+  }
+
+  toggleEditMode() {
+    this.service.editMode ? this.service.editMode = false : this.service.editMode = true
+  }
+
+  toggleTableMode() {
+    this.service.tableMode ? this.service.tableMode = false : this.service.tableMode = true
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { ItemComponent } from './item/item.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { EditComponent } from './edit/edit.component';
+import { EditListComponent } from './edit-list/edit-list.component'
+import { ItemsServiceService } from './items-service.service';
+import { Item2Component } from './item2/item2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ItemListComponent,
+    EditComponent,
+    EditListComponent,
+    Item2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ItemsServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
